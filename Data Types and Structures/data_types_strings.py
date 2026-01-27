ff3d0e5953f837d62cb674558229d877c69ad8bd
freerbot <danny_Freer@aishk.edu.hk>
2026-01-27 10:39:27 +0800

    First

--- Data Types and Structures/data_types_strings.py
+++ Data Types and Structures/data_types_strings.py
@@ -1,9 +1,18 @@
 #strings
+# Strings
+#
+# Store sequences of characters (letters, digits, symbols).
+# - Commonly used for text, names, messages, and identifiers.
+# - Often support operations such as concatenation and slicing.
+# - Typically enclosed in quotation marks.
+
+
 player_name = "Zorkon"
 player_title = " the Great"
+
 #below will concatenate (join) two strings into one
-player_full_name = player_name + player_title
-print(player_full_name)
+# player_full_name = player_name + player_title
+# print(player_full_name)
 # print(type(player_name))
 
 
