2d9ed71c77c65b97ef04f5d8f96f0d87f2dcc0b2
freerbot <danny_Freer@aishk.edu.hk>
2026-02-09 11:01:33 +0800

    First

---
+++ Functions/making_your_own_functions.py
@@ -0,0 +1,45 @@
+
+# This defines a function called say_hi.
+# It does not take any inputs (no parameters).
+def say_hi():
+    # This line runs when the function is called.
+    print("Hi, I'm a computer.")
+
+# This calls the function so the code inside it will run.
+say_hi()
+
+
+
+
+# # This defines a function that expects 1 parameter: name.
+# def say_hi(name):
+#     # This line keeps the value passed in the parameter.
+#     # (Not strictly needed, but shows how parameters are used.)
+#     name = name
+#
+#     # Uses an f-string to insert the name into the message.
+#     print(f"Hi, {name}")
+#
+# # A variable is created and given a value.
+# name = "Mr Freer"
+#
+# # The function is called and the value in 'name' is passed into it.
+# say_hi(name)
+
+
+
+# # This defines a function that expects 2 parameters: name and favourite_food.
+# def say_hi(name, favourite_food):
+#     # Stores the parameters in local variables (again, showing the process)
+#     name = name
+#     food = favourite_food
+#
+#     # Prints a message using both pieces of data.
+#     print(f"Hi, {name}. I like {food} also.")
+#
+# # Creating variables to hold the data to pass to the function.
+# name = "Mr Freer"
+# favourite_food = "pizza"
+#
+# # Calling the function with two arguments.
+# say_hi(name, favourite_food)
